04407b7fa07af77a50cf6d9bef9eecc4417e1488
goldipl <mgodlewski92@gmail.com>
2022-01-01 21:16:39 +0100

    handle states

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const pingPongCanvas = document.getElementById("pingPongCanvas");
 const ctx = pingPongCanvas.getContext("2d");
+const p1points = document.querySelector("span.p1points");
+const p2points = document.querySelector("span.p2points");
 
 const canvasHeight = pingPongCanvas.height;
 const canvasWidth = pingPongCanvas.width;
@@ -16,11 +18,12 @@ const ballStart_X = canvasWidth / 2;
 const ballStart_Y = canvasHeight / 2;
 const ballSpeedStart_X = 3;
 const ballSpeedStart_Y = 2;
+const changeState = 20;
 
 let ball_X = ballStart_X;
 let ball_Y = ballStart_Y;
-let ballSpeed_X = ballSpeedStart_X;
-let ballSpeed_Y = ballSpeedStart_Y;
+let ballPosition_X = ballSpeedStart_X;
+let ballPosition_Y = ballSpeedStart_Y;
 let P1points = 0;
 let P2points = 0;
 
@@ -45,3 +48,27 @@ drawBall(ballStart_X, ballStart_Y, ball_R);
 clearCanvas = () => {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 }
+
+drawActualState = () => {
+    clearCanvas();
+    drawPoints(P1points.toString(), p1points);
+    drawPoints(P2points.toString(), p2points);
+    drawBall(ball_X, ball_Y);
+    drawPaddle(paddleP1_X, paddleP1_Y);
+    drawPaddle(paddleP2_X, paddleP2_Y);
+}
+
+updateState = () => {
+    ball_X += ballPosition_X;
+    ball_Y += ballPosition_Y;
+
+    paddleP1_Y++;
+    paddleP2_Y++;
+    P1points++;
+    P2points += 3;
+}
+
+setInterval(updateStateAndDrawState = () => {
+    updateState();
+    drawActualState();
+}, changeState);
